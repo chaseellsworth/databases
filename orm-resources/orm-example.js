cd8ab319a1204140373922767e278e98ac7bd875
chaseme3 <chaseme3@gmail.com>
2014-11-21 19:11:22 -0800

    set up SQL but not yet connected to the app

--- orm-resources/orm-example.js
+++ orm-resources/orm-example.js
@@ -1,4 +1,4 @@
-/* You'll need to
+cv20v  v0c /* You'll need to
  * npm install sequelize
  * before running this example. Documentation is at http://sequelizejs.com/
  */
--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,47 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int(11) NOT NULL auto_increment,
+  user_id VARCHAR(100),
+  room_id VARCHAR(100),
+  text VARCHAR(500),
+  createdAt TIMESTAMP,
+  PRIMARY KEY (id),
+  INDEX (user_id),
+  INDEX (room_id)
+);
+
+CREATE TABLE users (
+  id int(11) NOT NULL auto_increment,
+  username VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id int(11) NOT NULL auto_increment,
+  roomname VARCHAR(100),
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
-
-
+-- CREATE TABLE collection (
+--   title VARCHAR(100),
+--   artist VARCHAR(100),
+--   genre VARCHAR(30),
+--   condition VARCHAR(10),
+--   worth DOUBLE(2,2),
+--   notes TEXT,
+--   released DATE,
+--   added DATE,
+--   opened ENUM('yes','no'),
+--   updated TIMESTAMP(14),
+--   ID int(11) NOT NULL auto_increment, PRIMARY KEY (ID)
+-- )
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < schema.sql
